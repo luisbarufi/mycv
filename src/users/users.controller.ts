06d2eb9985d51aafeac040f920e6a44cdacdbd2b
Luis Barufi <luisbarufi@gmail.com>
2024-06-11 00:00:45 -0300

    A Few Notes on Exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,7 +6,8 @@ import {
   Patch,
   Param,
   Query,
-  Delete
+  Delete,
+  NotFoundException
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +23,14 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+
+    if(!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
   @Get()
